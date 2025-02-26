c11bb7700a69ac03db59ccc40999a816cab70529
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-25 22:21:05 -0300

    Exercicio 1 - typescript

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ console.log(mensagem);
 let valores: number[] = [10, 20, 30];
 let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
 console.log("Soma dos valores:", somaValores);
+
+
+// Exercicio 1
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
