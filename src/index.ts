0ea57b27fc892ad8dba3908f446b03e2130a5424
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-25 22:22:55 -0300

    Exercicio 2 - Typescrip

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 let mensagem: string = "Olá, TypeScript!";
 console.log(mensagem);
 
-let valores: number[] = [10, 20, 30];
-let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
-console.log("Soma dos valores:", somaValores);
-
 
 // Exercicio 1
 let valores: number[] = [10, 20, 30];
 let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
 console.log("Soma dos valores:", somaValores);
+
+// Exercicio 2
+
+function mensagemPersonalizada(nome: string): string {
+    return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
+}
+console.log(mensagemPersonalizada("Ana"));
